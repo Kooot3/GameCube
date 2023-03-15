18a1818811379a02b89fc99749ec94ed9540af14
Kooot3 <124166824+Kooot3@users.noreply.github.com>
2023-03-15 22:35:33 +0300

    Start projetc

---
+++ Assets/Scripts/GameStateManager.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class GameStateManager : MonoBehaviour
+    {
+        private static GameStateManager _instance;
+        // Singleton pattern
+        public static GameStateManager Instance => _instance;
+
+        private bool _isDead = false;
+        private GameObject _player;
+
+        private void Awake()
+        {
+            if (_instance != null)
+            {
+                Destroy(_instance.gameObject);
+                return;
+            }
+            
+            _instance = this;
+        }
+
+        private void Start()
+        {
+            _player = FindObjectOfType<PlayerInput>().gameObject;
+        }
+
+        public void Die()
+        {
+            Destroy(_player);
+            _isDead = true;
+        }
+    }
+}
