edd3ff616dfd94b4a359802d090ec81044d60515
Kooot3 <124166824+Kooot3@users.noreply.github.com>
2023-03-20 19:08:26 +0300

    Use DOTween

---
+++ Assets/Scripts/Cube/CameraPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using UnityEngine;
+
+namespace DefaultNamespace
+{
+    public class CameraPlayer : MonoBehaviour
+    {
+        public GameObject player;
+        private Vector3 offset;
+
+        private void Start()
+        {
+            offset = transform.position;
+        }
+
+        private void LateUpdate()
+        {
+            transform.position = player.transform.position + offset;
+        }
+    }
+}
---
+++ Assets/Scripts/EventObjects/MovingPlatformDirected.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using DefaultNamespace;
+using DefaultNamespace.EventObjects;
+using UnityEngine;
+
+public class MovingPlatformDirected : MovingPlatform
+{
+    [SerializeField] private Vector3 _direction = new Vector3(0, 0, -0.5f);
+
+
+    public override void Move()
+    {
+        transform.position += _direction * Time.deltaTime;
+    }
+}
